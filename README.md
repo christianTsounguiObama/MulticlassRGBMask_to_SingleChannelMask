b83f61693a146baf6a5559a6b51862abc23021a3
christianTsounguiObama <christian.tsoungui.obama@gmail.com>
2024-03-24 15:31:21 +0100

    update

--- README.md
+++ README.md
@@ -1,2 +1,11 @@
 # MulticlassRGBMask_to_SingleChannelMask
-This repository contains a sample python script that can be used for the conversion of a multiclass RGB image into a single channel grayscale image, used to train computer vision models.
+This script is aimed at helping with the preparation of RGB masks for use to train computer vision models
+for semantic segmentation.
+
+The RGB masks that label each class in a picture of interest by a specified color are converted into
+a single channel (more precisely, 3 identical channels) mask and saved in a given folder. 
+
+We thank all the individuals who shared pieces of codes which inspired this script. Feel free to use, 
+share, and modify as you wish :)
+
+Usage: python3 rgbmask_to_singlechannelmask.py
--- src/rgbmask_to_singlechannelmask.py
+++ src/rgbmask_to_singlechannelmask.py
@@ -1,64 +1,98 @@
+#!/usr/bin/env python3
+
+'''
+    This script is aimed at helping with the preparation of RGB masks for use to train computer vision models
+    for semantic segmentation.
+    The RGB masks that label each class in a picture of interest by a specified color are converted into
+    a single channel (more precisely, 3 identical channels) mask and saved in a given folder. 
+    
+    We thank all the individuals who shared pieces of codes which inspired this script. Feel free to use, 
+    share, and modify as you wish :)
+
+    Usage: python3 rgbmask_to_singlechannelmask.py
+'''
+
 import os
 import cv2
 import PIL.Image, PIL.ImageFont, PIL.ImageDraw
 import numpy as np
 import pathlib
 import pandas as pd
+import warnings
 
-def prep_mask(mask, colormap):
-    output_mask = []
-    for i, color in enumerate(colormap):
-        cmap = np.all(np.equal(mask, color),  axis=-1)
-        output_mask.append(cmap)
+def loadColormapFromCSV(inputfolder):
+    '''
+        This function loads a csv file that contains the color (r, g, b)
+        represeting each class in the in the RGB mask.
 
-    output_mask = np.stack(output_mask, axis=-1)
-    return output_mask
+        inputfolder = Path to the csv file
+    '''
+    class_names_read = pd.read_csv(inputfolder)
+    colormap = np.array(class_names_read.iloc[:,1:])
 
-def convert_to_grayscale_mask(input_folder, output_folder, colormap):
-    # Ensure the output folder exists
-    if not os.path.exists(output_folder):
-        os.makedirs(output_folder)
+    return colormap
 
-    # Get list of image filenames in the input folder
-    dir = pathlib.Path(input_folder)
-    image_files = list(dir.glob('**/*.png'))
+def maskOneHotEncoding(mask, classcolorfilefolder):
+    '''
+        This function converts the RGB mask into a list of length equal the number 
+        of classes and the i-th elemnt of the list is a 0-1 matrix in which the pixels
+        containing the i-th class in the RGB mask have the value 1 while the remaining
+        pixels have value 0 
+
+        mask = RGB mask image
+        classcolorfilefolder = Folder in which the CSV file containing the class-color mapping is stored.
+    '''
+    colormap = loadColormapFromCSV(classcolorfilefolder)
+    outputmask = [np.all(np.equal(mask, color),  axis=-1) for color in colormap]
+    outputmask = np.stack(outputmask, axis=-1)
+
+    return outputmask
+
+def singleChannelMaskConversion(inputfolder, outputfolder, classcolorfilefolder):
+    '''
+        This function converts the one hot encoded mask into a single channel mask 
+        (3 identical channels to be more precise), and saves the masks in the specified 
+        output folder. If the folder does not exist, it will be created.
+
+        inputfolder = Folder in which the RGB masks are stored.
+        outputfolder = Folder in which the single channel masks will be stored.
+        classcolorfilefolder = Folder in which the CSV file containing the class-color mapping is stored
+    '''
     
-    for img_file in image_files:
-        # Read the RGB mask using OpenCV        
-        img = PIL.Image.open(img_file)
-        img = np.asarray(img)
+    if not os.path.exists(outputfolder):
+        warnings.warn("Folder not found, it will be created for you.")
+        os.makedirs(outputfolder)
+
+    rgbmaskfolder = pathlib.Path(inputfolder)
+    rgbmaskpathlist = list(rgbmaskfolder.glob('**/*.png'))
+    
+    for rgbmaskpath in rgbmaskpathlist:        
+        rgbmask = PIL.Image.open(rgbmaskpath)
+        rgbmask = np.asarray(rgbmask)
         
-        # Convert to multiclass grayscale image
-        output_mask = []
-        for i, color in enumerate(colormap):
-            cmap = np.all(np.equal(img, color),  axis=-1)
-            output_mask.append(cmap)
+        outputmask = maskOneHotEncoding(rgbmask, classcolorfilefolder)
+
+        # Create array in which each entry (corresponding to image pixels) contains 
+        # the class label number, e.g., 5 for the class 'car'
+        singlechannelmask = np.argmax(outputmask, axis=-1)
+        singlechannelmask = np.expand_dims(singlechannelmask, axis=-1)
+
+        # Create mask as a 3 channel image for which all channels are identical
+        mask = np.concatenate([singlechannelmask, singlechannelmask, singlechannelmask], axis=-1)
         
-        output_mask = np.stack(output_mask, axis=-1)
-        grayscale_mask = np.argmax(output_mask, axis=-1)
-        
-        grayscale_mask = np.expand_dims(grayscale_mask, axis=-1)
-        mask = np.concatenate([grayscale_mask, grayscale_mask, grayscale_mask], axis=-1)
-        img_path = str(img_file)[(len(input_folder)+1):]
-        
-        output_path = os.path.join(output_folder, img_path)
+        # Pick rgbmask name (with extension, e.g., 'png')
+        rgbmaskname = str(rgbmaskpath)[(len(inputfolder)+1):]
+        output_path = os.path.join(outputfolder, rgbmaskname)
+
+        # Save single channel mask to output folder with same name as rgb mask
         cv2.imwrite(output_path, mask)
 
-        print(f"Converted {img_file} to grayscale and saved as {output_path}")
+    print(f"All files successfully converted to single chanel masks. Find them in: {outputfolder}")
 
 # Input and output folder paths
-input_folder = "/home/christian/Documents/Robotics/DNN/dataset/CamVid/test_labels"
-output_folder = "/home/christian/Documents/Robotics/DNN/dataset/CamVid/test_labels_prepped"
-
-#Loading class labels dataset
-class_names_read = pd.read_csv('/home/christian/Documents/Robotics/DNN/dataset/CamVid/class_dict.csv')
-
-# pixel labels in the video frames
-class_names = np.array(class_names_read['name']).T
-class_names = class_names.tolist()
-
-# Labels colormap
-colormap = np.array(class_names_read)[:,1:]
+labelsdir = "/home/christian/Documents/Artifitial_Intelligence/MulticlassRGBMask_to_SingleChannelMask/dataset/train_labels"
+preppedlabelsdir = "/home/christian/Documents/Artifitial_Intelligence/MulticlassRGBMask_to_SingleChannelMask/dataset/train_labels_prepped"
+classcolorfilefolder = '/home/christian/Documents/Artifitial_Intelligence/MulticlassRGBMask_to_SingleChannelMask/dataset/class_dict.csv'
 
 # Convert images to grayscale
-convert_to_grayscale_mask(input_folder, output_folder, colormap)
+singleChannelMaskConversion(labelsdir, preppedlabelsdir, classcolorfilefolder)
